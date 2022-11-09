d23b9fe5a775bd524ad6b2d1226eec50362c2b88
Student <pavanmpatel20@gnu.ac.in>
2022-11-09 14:56:47 +0530

    Initial commit

--- app/src/main/java/com/example/contextmenuexample/MainActivity.kt
+++ app/src/main/java/com/example/contextmenuexample/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.contextmenuexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import com.example.contextmenuexample.databinding.ActivityMainBinding
+import com.example.contextmenuexample.databinding.ContentMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val contact = arrayOf<String>("Akash","Vikash","John","Rahul","Ajay")
+    private lateinit var listView: ListView
+    private lateinit var binding: ContentMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ContentMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        setSupportActionBar(toolbar)
+
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contact)
+        listView = binding.listView
+        listView.adapter = arrayAdapter
+        registerForContextMenu(listView)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item!!.itemId) {
+            R.id.call ->{
+                Toast.makeText(applicationContext, "call code", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.sms ->{
+                Toast.makeText(applicationContext, "sms code", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
